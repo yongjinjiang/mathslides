c0981314455bcb9cb9e680b23ac48c8c7b1bb42a
jyj <yjjiangphysics@gmail.com>
2020-08-26 22:16:56 -0500

    Updates

---
+++ precache-manifest.9ae7b4bf0a312b636203cbfad05a4665.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e3fa9f77e864bf93ea65985852e62c37",
+    "url": "/mathslides/index.html"
+  },
+  {
+    "revision": "3338cc1cd9ed7113bc34",
+    "url": "/mathslides/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "a9e6f8aeb32be8cb45e6",
+    "url": "/mathslides/static/js/2.6acee0e9.chunk.js"
+  },
+  {
+    "revision": "f74a6debb7da89320918198b818026f8",
+    "url": "/mathslides/static/js/2.6acee0e9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3338cc1cd9ed7113bc34",
+    "url": "/mathslides/static/js/main.8d6486e5.chunk.js"
+  },
+  {
+    "revision": "b8f8ff0b7e9fecbcf5b4",
+    "url": "/mathslides/static/js/runtime-main.01c671e0.js"
+  }
+]);
